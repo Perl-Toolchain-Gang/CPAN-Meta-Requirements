2bc02c5f7833822fdd8711fc9a310029fe31e80d
David Golden <xdg@xdg.me>
2015-12-11 12:43:09 -0500

    After release: bump $VERSION and timestamp Changes

--- lib/CPAN/Meta/Requirements.pm
+++ lib/CPAN/Meta/Requirements.pm
@@ -4,7 +4,7 @@ use warnings;
 package CPAN::Meta::Requirements;
 # ABSTRACT: a set of version requirements for a CPAN dist
 
-our $VERSION = '2.139'; # TRIAL
+our $VERSION = '2.140';
 
 =head1 SYNOPSIS
 
