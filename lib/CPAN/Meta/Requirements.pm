8246086e6f3f4d45f304cbd5dbbffc9ad3e31e0d
Ricardo Signes <rjbs@semiotic.systems>
2023-05-09 07:15:30 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/CPAN/Meta/Requirements.pm
+++ lib/CPAN/Meta/Requirements.pm
@@ -4,7 +4,7 @@ use warnings;
 package CPAN::Meta::Requirements;
 # ABSTRACT: a set of version requirements for a CPAN dist
 
-our $VERSION = '2.142';
+our $VERSION = '2.143';
 
 use CPAN::Meta::Requirements::Range;
 
--- lib/CPAN/Meta/Requirements/Range.pm
+++ lib/CPAN/Meta/Requirements/Range.pm
@@ -4,7 +4,7 @@ use warnings;
 package CPAN::Meta::Requirements::Range;
 # ABSTRACT: a set of version requirements for a CPAN dist
 
-our $VERSION = '2.142';
+our $VERSION = '2.143';
 
 use Carp ();
 
